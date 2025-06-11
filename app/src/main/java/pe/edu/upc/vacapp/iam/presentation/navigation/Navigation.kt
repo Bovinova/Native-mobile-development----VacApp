98f2c2e8d4d377356ad9473de2801b9cbcd3f406
Franco Durand <francoduve.25@gmail.com>
2025-06-11 13:05:23 -0500

    feat: implement navigation and login screen with UI components

---
+++ app/src/main/java/pe/edu/upc/vacapp/iam/presentation/navigation/Navigation.kt
@@ -0,0 +1,34 @@
+package pe.edu.upc.vacapp.iam.presentation.navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.upc.vacapp.iam.presentation.view.Login
+import pe.edu.upc.vacapp.ui.theme.Color
+
+@Preview
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.background(Color.LightGray)
+    ) { padding ->
+        NavHost(
+            navController,
+            startDestination = "login",
+            modifier = Modifier.padding(padding)
+        ) {
+            composable("login") {
+                Login()
+            }
+
+        }
+    }
+}
